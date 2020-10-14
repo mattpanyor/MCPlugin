3e63d7856929b27bde432833a8af05740d121b56
PANYOR <m2@panyor.com>
2020-10-14 14:24:16 +0100

    unused import

--- src/main/java/pyr/solutions/allkingplugin/events/OpMessage.java
+++ src/main/java/pyr/solutions/allkingplugin/events/OpMessage.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.server.BroadcastMessageEvent;
 
 public class OpMessage implements Listener {
 
